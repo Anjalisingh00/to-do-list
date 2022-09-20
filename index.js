af985ceb11c47545ce571c0c6f5b829d5ac76445
Anjalisingh00 <anjalisinghftp0000@gmail.com>
2022-09-20 20:07:26 +0530

    ToDo List

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+//import List from './List'
+import App from "./App";
+
+
+
+
+
+ReactDom.render(
+
+<App/>,
+ document.getElementById("root")
+
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
